948f1af876dcd22567f4da985752340bfebdda7e
thomas cogez <tom.cogez-allix@etu.univ-lehavre.fr>
2019-06-17 10:19:24 +0200

    initPlateau

---
+++ InitPlateau.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * InitPlateau
+ */
+public class InitPlateau {
+    private int nbJoueur;
+    private File fichier;
+    private Scanner sc;
+    public InitPlateau(int nbJoueur) {
+        this.nbJoueur = nbJoueur;
+        
+        try {
+            fichier = new File("niveaux/n"+ nbJoueur +".data");
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void lireNiveau(){
+        try {
+            sc = new Scanner(fichier);
+            while (sc.hasNextLine()){
+            
+            }
+            
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    
+        
+    }
+}
