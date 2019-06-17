ef65c56851a0a06ae8fb46e32a21f30ad30dfb36
LeVengeurMasque <mathieu.boireau@etu.univ-lehavre.fr>
2019-06-17 10:29:18 +0200

    .

---
+++ CaseHexa.java
@@ -0,0 +1,37 @@
+public class CaseHexa
+{
+	private CaseHexa[] casesVoisines;
+	private IContenu   contenuCase;
+
+	public CaseHexa(CaseHexa[] casesVoisines)
+	{
+		for(int i=0; i<6; i++)
+			this.casesVoisines[i] = casesVoisines[i];
+		contenuCase = null;
+	}
+
+	public CaseHexa()
+	{
+		this(new CaseHexa[6]);
+	}
+
+	public void setVoisin(int cote, CaseHexa autreCase)
+	{
+		if(0<=cote && cote<6 &&
+		   autreCase != null && autreCase.casesVoisines[(cote+3)%6] == null)
+		{
+			this.casesVoisines[cote] = autreCase;
+			autreCase.casesVoisines[(cote+3)%6] = this;
+		}
+	}
+
+	public void setContenu(IContenu contenuCase)
+	{
+		this.contenuCase = contenuCase;
+	}
+
+	public IContenu getContenu()
+	{
+		return contenuCase;
+	}
+}
---
+++ Cristal.java
@@ -0,0 +1,21 @@
+public class Cristal implements IContenu
+{
+	private int valeur;
+
+	private Cristal(int valeur)
+	{
+		this.valeur = valeur;
+	}
+
+	public Cristal creerCristal(int valeur)
+	{
+		if(valeur>=2 && valeur<=4)
+			return this(valeur);
+		return null;
+	}
+
+	public int getValeur()
+	{
+		return valeur;
+	}
+}
