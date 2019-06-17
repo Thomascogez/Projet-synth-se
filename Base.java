4beb01944435df8c5f3a2d388b845a9e9a806cc7
thomas cogez <tom.cogez-allix@etu.univ-lehavre.fr>
2019-06-17 10:39:32 +0200

    ajout base

---
+++ Base.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+/**
+ * Base
+ */
+public class Base {
+
+    private ArrayList<Cristal> cristaux;
+
+    public Base() {
+        this.cristaux = new ArrayList <Cristal>();
+    }
+    /**
+     * Methode permettant d'ajouter un crystal dans la base
+     * 
+     * @param c la crystal a ajouter
+     */
+    public void ajouterCristal(Cristal c){
+        this.cristaux.add(c);
+    }
+
+    public ArrayList<Cristal> getCristaux() {
+        return cristaux;
+    }
+
+    @Override
+    public String toString() {
+        return "Cristaux dans la base "+cristaux;
+    }
+
+   
+
+}
