73d095e3b012a4b1a45a86b65921e301ee46d5bb
Thomascogez <44041651+Thomascogez@users.noreply.github.com>
2019-06-17 16:25:33 +0200

    Update IHMCUI.java

--- IHMCUI.java
+++ IHMCUI.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+
 import iut.algo.*;
 public class IHMCUI
 {
-	public IHMCUI()
-	{
 
-	}
+	public void ecranDemarrage() 
+	{
+        System.out.println(
+                "  _   _   _   _     _   _   _     _   _   _   _  \r\n / \\ / \\ / \\ / \\   / \\ / \\ / \\   / \\ / \\ / \\ / \\ \r\n( T | w | i | n ) ( T | i | n ) ( B | o | t | s )\r\n \\_/ \\_/ \\_/ \\_/   \\_/ \\_/ \\_/   \\_/ \\_/ \\_/ \\_/ ");
+    }
 
 	public String[] nouvellePartie()
 	{
@@ -14,15 +18,17 @@ public class IHMCUI
 
 		while (nbJoueurMax>6||nbJoueurMax<2)
 		{
-			System.out.println ( "ERREUR : Choix du nombre de joueurs invalide !" );
-			System.out.print ( "Choix du nombre de joueurs [2..6] : " );
+			Console.println ( CouleurConsole.ROUGE.getFont()"ERREUR : Choix du nombre de joueurs invalide !" );
+			Console.normal();
+			Console.print ( "Choix du nombre de joueurs [2..4] : " );
 			nbJoueurMax = Clavier.lire_int();
 		}
 		//...Et on demande leurs noms respectifs
 		String[] tabNom = new String[nbJoueurMax];
 		for (int i= 0; i< nbJoueurMax ;i++ )
 		{
-			System.out.print ("Nom du Joueur " +(i+1)+ " : ");
+			Console.print (CouleurConsole.CYAN.getFont()"Nom du Joueur " +(i+1)+ " : ");
+			Console.normal();
 			tabNom[i] = Clavier.lireString();
 		}
 		return tabNom;
@@ -31,5 +37,25 @@ public class IHMCUI
 	public void afficherGrille(String grille)
 	{
 		System.out.println(grille);
+	}
+	public void afficherScores(ArrayList<Joueur> lJoueur)
+	{
+        System.out.println("+------------+-----------+");
+        System.out.println("|      Joueur|      Score|");
+        System.out.println("+------------+-----------+");
+        for (Joueur j : lJoueur) 
+			Console.println(String.format("|"+CouleurConsole.VERT.getFont()+" %10s"+CouleurConsole.JAUNE.getFont()+" | "+CouleurConsole.JAUNE.getFont()+"%3d points"+CouleurConsole.BLANCE.getFont()+"|", j.getNom(), j.getScore()));
+			
+		Console.normal();
+        System.out.println("+------------+-----------+");
+        
+	}
+	public void menuAction() {
+        Console.println("\tVoulez-vous modifier un programme avant execution ? : \n\n" + "\t\t"+ CouleurConsole.VERT.getFont() +" 1 - Oui\n" + "\t\t"+ CouleurConsole.ROUGE.getFont() +" 2 - Non");
+        Console.normal();
+    }
+	
+	public void finDePartie(){
+		Console.println(CouleurConsole.JAUNE.getFont()+"  _   _   _     _   _     _   _   _   _   _   _  \r\n / \\ / \\ / \\   / \\ / \\   / \\ / \\ / \\ / \\ / \\ / \\ \r\n( F | i | n ) ( d | e ) ( P | a | r | t | i | e )\r\n \\_/ \\_/ \\_/   \\_/ \\_/   \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ ");
 	}
 }
