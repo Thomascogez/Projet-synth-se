9de82884fc3b890aea4910f101d3b147d5cfb341
thomas cogez <tom.cogez-allix@etu.univ-lehavre.fr>
2019-06-19 09:31:26 +0200

    test chargement config

---
+++ tests/TestChargementTest.java
@@ -0,0 +1,25 @@
+
+
+/**
+ * TestChargementTest
+ */
+public class TestChargementTest {
+
+    public static void main(String[] args) {
+        String[] tabJoueur = {"Joueur1","Joueur2"};
+        ChargementTest ct = new ChargementTest();
+      
+        
+
+        
+
+        if((ct.getProperty("JOUEUR1_ROBOT1_LIST_ACTION").trim()).equals("TRUE"))
+
+            
+       
+        for (int i = 0; i < tabJoueur.length; i++) {
+            
+            
+        }
+    }
+}
